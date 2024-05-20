4bc11e016d778af383b3b4703d9de43f070a8cb4
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2024-05-20 04:30:43 -0400

    added validity checks to the do_sth_with_PlayerParty function

--- q1_q4/q3_sth_party.lua
+++ q1_q4/q3_sth_party.lua
@@ -1,10 +1,30 @@
 function do_sth_with_PlayerParty(playerId, membername)
     player = Player(playerId)
+    -- validate the player table
+    if not player then
+        print("Error: player Id"..playerId.."is invalid")
+        return
+    end
+
     local party = player:getParty()
-    
-    for k,v in pairs(party:getMembers()) do
-        if v == Player(membername) then
-            party:removeMember(Player(membername))
+
+    if not party then
+        print("Error: Player is not in party")
+        return
+    end
+
+    local member = Player(membername)
+    -- validate the memeber table
+    if not member then
+        print("Error: membername "..member.."is invalid")
+        return
+    end
+
+    for _, partymember in pairs(party:getMembers()) do
+        if partymember == member then
+            party:removeMember(member)
+            return
         end
     end
+    print("member "..membername.." not in player's party")
 end
